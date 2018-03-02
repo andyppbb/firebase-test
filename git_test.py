7e8559a7803ea7d204b9630a0611534ca9082632
Andy Chang <andychang@ifunplay.com>
2018-03-02 15:14:52 +0800

    2018-03-02 15:14:52.880444

---
+++ git_test.py
@@ -0,0 +1,13 @@
+from github import Github
+from datetime import datetime
+g = Github("andyppbb@gmail.com", "jack7820")
+for repo in g.get_user().get_repos():
+    print(repo.name)
+
+from git import Repo
+repo_dir = ''
+repo = Repo(repo_dir)
+repo.git.add(A=True)
+repo.index.commit(str(datetime.now()))
+origin = repo.remote('origin')
+origin.push()
