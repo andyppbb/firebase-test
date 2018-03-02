33bade70650f3010982b42f60e7cdc4af9a177b7
Andy Chang <andychang@ifunplay.com>
2018-03-02 15:20:34 +0800

    2018-03-02 15:20:34.630169

--- git_test.py
+++ git_test.py
@@ -1,9 +1,11 @@
+'''
 from github import Github
-from datetime import datetime
+
 g = Github("andyppbb@gmail.com", "jack7820")
 for repo in g.get_user().get_repos():
     print(repo.name)
-
+'''
+from datetime import datetime
 from git import Repo
 repo_dir = ''
 repo = Repo(repo_dir)
